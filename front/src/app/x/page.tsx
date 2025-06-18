2245c996e42bf259936042b01960aeda7bfdd40f
vuthanhtung2412 <vuthanhtung2016hn@gmail.com>
2025-06-18 14:02:30 +0200

    sample page

---
+++ front/src/app/x/page.tsx
@@ -0,0 +1,8 @@
+'use client'
+import Counter from '@/components/Counter';
+
+export default function X() {
+  return (
+    <Counter />
+  );
+}
---
+++ front/src/components/Counter.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { useCounter } from '@/hooks/useCounter';
+
+
+export default function Counter() {
+  const { count, increase, decrease } = useCounter();
+
+  return (
+    <div>
+      <button onClick={decrease}>Decrease</button>
+      <br />
+      <button onClick={increase}>Increase</button>
+      <p>Count: {count}</p>
+    </div>
+  );
+}
