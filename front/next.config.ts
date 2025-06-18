1113420fd9b7b210ac115bf6e326c1e5d5b98531
vuthanhtung2412 <vuthanhtung2016hn@gmail.com>
2025-06-18 16:58:25 +0200

    routing asset

--- front/next.config.ts
+++ front/next.config.ts
@@ -1,13 +1,15 @@
 import type { NextConfig } from "next";
+import { GITHUB_PROJECT } from "@/const";
 
 const isGitHubPages = process.env.GITHUB_PAGES === 'true'; // Set this in your build process
 
 const nextConfig: NextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  basePath: isGitHubPages ? '/RAISE-AI-hackathon' : '',
-  assetPrefix: isGitHubPages ? '/RAISE-AI-hackathon/' : '',
-  trailingSlash: true, // This can help with GitHub Pages
+  basePath: isGitHubPages ? `/${GITHUB_PROJECT}` : '',
+  // Why I don't use `assetPrefix`?
+  // https://nextjs.org/docs/app/api-reference/config/next-config-js/assetPrefix#:~:text=an%20Asset%20Prefix.-,Good%20to%20know%3A,-Next.js%209.5
+  trailingSlash: true, 
 };
 
 export default nextConfig;
--- front/src/const.ts
+++ front/src/const.ts
@@ -1,1 +1,2 @@
 export const BACKEND_URL = "http://localhost:8000/";
+export const GITHUB_PROJECT = "RAISE-AI-hackathon"
