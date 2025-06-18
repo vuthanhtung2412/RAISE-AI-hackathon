9cda845f0544b3aa2047ae252aee983b3e9ccd84
vuthanhtung2412 <vuthanhtung2016hn@gmail.com>
2025-06-18 16:13:05 +0200

    minor chage

--- front/next.config.ts
+++ front/next.config.ts
@@ -1,13 +1,12 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
 const isGitHubPages = process.env.GITHUB_PAGES === 'true'; // Set this in your build process
 
 const nextConfig: NextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  basePath: (isProd && isGitHubPages) ? '/RAISE-AI-hackathon' : '',
-  assetPrefix: (isProd && isGitHubPages) ? '/RAISE-AI-hackathon/' : '',
+  basePath: isGitHubPages ? '/RAISE-AI-hackathon' : '',
+  assetPrefix: isGitHubPages ? '/RAISE-AI-hackathon/' : '',
   trailingSlash: true, // This can help with GitHub Pages
 };
 
